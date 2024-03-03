3b6a48a839d47fb417928b28695c930dc69e9d72
Max <Max>
2024-03-03 16:45:16 +0300

    finished 1st task

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Main(){
+    Console.WriteLine("Введите значение M:");
+    int m = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите значение N:");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    Recursion(m, n);
+
+}
+
+void Recursion(int m, int n){
+    if (m > n){
+        Console.Write(n + " ");
+        return;
+    }
+
+    Console.Write($"{m} ");
+    Recursion(m + 1, n);
+}
+
+Main();
