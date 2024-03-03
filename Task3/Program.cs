703be15aeb7aa75ca5dcadd14421098a9a9e4f2b
Max <Max>
2024-03-03 17:14:59 +0300

    3rd task complete

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void Main(){
+
+int[] array = {1,2,3,4,5};
+
+PrintArray(array.Length, array);
+
+}
+
+void PrintArray(int i, int[] array){
+
+    if (i == 0) 
+        return; 
+
+    Console.Write($" {array[i - 1]} "); 
+
+    PrintArray(--i, array);
+
+}
+
+Main();
+
+
